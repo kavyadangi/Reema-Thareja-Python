6dc7b1e0489e4794179485be65097824c9e9131a
Kavya <kavyadangi09@gmail.com>
2023-05-01 12:48:32 +0530

    Create obtaining_grades.py

---
+++ Unit 1/obtaining_grades.py
@@ -0,0 +1,11 @@
+M=int(input("enter mrks"))
+if(M>75):
+    print("O")
+elif(M>=60 and M<=75):
+    print("A")
+elif(M>=50 and M<60):
+    print("B")
+elif(M>=40 and M<50):
+    print("C")
+else:
+    print("D")
